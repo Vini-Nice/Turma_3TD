33f293ac27b666501e9d0eabb6a46fcd6c39ee47
Vini-Nice <viniciussantanapsn@gmail.com>
2025-08-08 14:56:57 -0300

    Aula 1

---
+++ tres_cores/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: { flex: 1 },
+    area1: {
+        flex: 1,
+        backgroundColor: '#8a2be2'
+    },
+    area2: {
+        flex: 5,
+        backgroundColor: '#cd7712',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    area3: {
+        flex: 1,
+        backgroundColor: '#0065da'
+    },
+    title: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        color: '#fffff',
+        textAlign: 'center',
+    }
+})
